8357a7212567acd0155c0896ab0bcf02182c6388
Shiraz Bhalla <shiraz.bhalla@copart.com>
2017-03-15 16:21:19 -0500

    merged

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,11 @@
 // we have installed the react in node modules
 // this gives file wide access to React
-import React, { Component }   from 'react'
-import HomeScreen             from './HomeScreen'
-
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-         <HomeScreen />
-      </div>
+      <div>React-Redux baseline by Shiraz Bhalla</div>
     );
   }
 }
